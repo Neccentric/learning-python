71ae6f784b8dc4268ecadcccd99d4e76b6c696a9
Neccentric <obiscondu@gmail.com>
2025-06-22 18:56:32 +0100

    Calculator-Completed

--- 01-foundation/week-01-getting-started/exercises/03_calculator.py
+++ 01-foundation/week-01-getting-started/exercises/03_calculator.py
@@ -11,3 +11,21 @@
 # Hint: Use f-strings for formatting output
 
 # Your code here:
+
+first_input = float(input("Enter the first number: "))
+second_input = float(input("Enter the second number: "))
+addition = first_input + second_input
+subtraction = first_input - second_input
+multiplication = first_input * second_input
+division = first_input / second_input if second_input!= 0 else "undefined (cannot divide by zero)"
+Modulus = first_input % second_input if second_input != 0 else "undefined (cannot divide by zero)"
+# Displaying the results
+print("\nResults:")
+print(f"Addition: {first_input} + {second_input} = {addition}")
+print(f"Subtraction: {first_input} - {second_input} = {subtraction}")
+print(f"Multiplication: {first_input} * {second_input} = {multiplication}")
+print(f"Division: {first_input} / {second_input} = {division}") 
+print(f"Modulus: {first_input} % {second_input} = {Modulus}")
+print(f"Exponentiation: {first_input} ** {second_input} = {first_input  ** second_input}")
+print(f"Floor Division: {first_input} // {second_input} = {first_input // second_input if second_input != 0 else 'undefined (cannot divide by zero)'}")
+print(f"Square Root of {first_input} = {first_input ** 0.5}")
