ab82e78d21ec674f6c71768df854dceb7d9d663d
Neccentric <obiscondu@gmail.com>
2025-06-22 18:25:54 +0100

    Variables-Completed

--- 01-foundation/week-01-getting-started/exercises/02_variables.py
+++ 01-foundation/week-01-getting-started/exercises/02_variables.py
@@ -2,13 +2,13 @@
 # TODO: Complete the following tasks
 
 # 1. Create a variable for your name
-name = ""  # Replace with your name
+name = "Ndu Emmanuel Obinna"  # Replace with your name
 
 # 2. Create a variable for your age
-age = 0  # Replace with your age
+age = 19  # Replace with your age
 
 # 3. Create a variable for your height in meters
-height = 0.0  # Replace with your height
+height = 1.8288  # Replace with your height
 
 # 4. Create a boolean variable indicating if you like Python
 likes_python = True  # True or False
@@ -17,3 +17,12 @@ likes_python = True  # True or False
 # Example: print("My name is:", name)
 
 # Your code here:
+print("My name is:", name)
+print("My age is:", age)
+print("My height in meters is:", height)
+print("Do I like Python?", likes_python)
+# 6. Print the data types of each variable
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of height:", type(height))
+print("Data type of likes_python:", type(likes_python))
